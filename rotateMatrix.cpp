a9c4518877ab6f6252f074c5fe95f4785c37c81f
Alby Thomas <albytho@umich.edu>
2017-06-26 22:15:23 -0400

    Add files via upload

---
+++ rotateMatrix.cpp
@@ -0,0 +1,48 @@
+//
+//  main.cpp
+//  rotateMatrix
+//
+//  Created by Alby Thomas on 6/26/17.
+//  Copyright © 2017 Alby Thomas. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void rotate(vector<vector<int>> &matrix){
+    int m = int(matrix.size());
+    
+    //Go layer by layer
+    for(int layer=0; layer<m/2; ++layer){
+        
+        //As you into a deeper layer, the first and last values get closer together
+        int first = layer;
+        int last = m-1-layer;
+        
+        //You need this for loop to account for the shifts in each of the layers
+        for(int i=first; i<last; ++i){
+            int offset = i-first;
+            int top = matrix[first][i];
+            
+            matrix[first][i] = matrix[last-offset][first];
+            matrix[last-offset][first] = matrix[last][last-offset];
+            matrix[last][last-offset] = matrix[first+offset][last];
+            matrix[first+offset][last] = top;
+        }
+    }
+    
+}
+
+int main() {
+    vector<vector<int>> matrix = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+    rotate(matrix);
+    
+    for(int row=0; row<matrix.size(); ++row){
+        for(int col=0; col<matrix[0].size(); ++col){
+            cout<<matrix[row][col]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
