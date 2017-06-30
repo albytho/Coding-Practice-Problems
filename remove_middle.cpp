00e14077e17c77da7f50d9626577a316e5349859
Alby Thomas <albytho@umich.edu>
2017-06-29 21:22:56 -0400

    Update remove_middle.cpp

--- remove_middle.cpp
+++ remove_middle.cpp
@@ -1,18 +1,8 @@
-void remove_middle(){
-    if(head->next != nullptr && head->next->next != nullptr){
-        Node *current = head;
-        Node *a = head;
-        int counter = 0;
-        while(current->next != nullptr){
-            current = current->next;
-            ++counter;
-        }
-        for(int index=0; index<(counter/2)-1; ++index){
-            a=a->next;
-        }
-        Node *temp = a->next;
-        a->next = temp->next;
-        delete temp;
-        temp = nullptr;
-    }
+void remove_middle(Node *middle){
+    middle->data = middle->next->data;
+        
+    Node *temp = middle->next;
+    middle->next = temp->next;
+    delete temp;
+    temp = nullptr;  
 }
